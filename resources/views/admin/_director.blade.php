be70b841020157b70b9a2d04799233aa56c80172
Michael Cuneo <cuneo24@yahoo.com>
2019-10-13 19:39:45 -0400

    10/13 - admin panel style fixes

--- resources/views/admin/_director.blade.php
+++ resources/views/admin/_director.blade.php
@@ -1,19 +1,22 @@
-<div class='directorBar'>
-    &nbsp;<strong>{{$director->fname}} {{$director->lname}}</strong>, <em>{{$director->position}}</em>
+<div class='directorWrapper'>
+    <div class='directorBar'>
+        &nbsp;<strong>{{$director->fname}} {{$director->lname}}</strong>, <em>{{$director->position}}</em>
+    </div>
+    <div class='directorInd'>
+        <br>
+        {{$director->description}}
+        <br><br>
+
+        <form method='get' class='adminForm' action='/admin/director/{{$director->id}}/edit'>
+            <input type='submit' class='adminButton editButton' value='Edit'>
+        </form>
+
+        <form method='get' class='adminForm' action='/admin/director/{{$director->id}}/delconfirm'>
+            {{csrf_field()}}
+            <input type='submit' class='adminButton deleteButton' value='Delete'>
+        </form>
+
+        <br><br>
+    </div>
 </div>
-<div class='directorInd'>
-    <br>
-    {{$director->description}}
-    <br><br>
-
-    <form method='get' class='adminForm' action='/admin/director/{{$director->id}}/edit'>
-        <input type='submit' class='adminButton editButton' value='Edit'>
-    </form>
-
-    <form method='get' class='adminForm' action='/admin/director/{{$director->id}}/delconfirm'>
-        {{csrf_field()}}
-        <input type='submit' class='adminButton deleteButton' value='Delete'>
-    </form>
-
-    <br><br>
-</div>
+<br>
--- resources/views/admin/_partner.blade.php
+++ resources/views/admin/_partner.blade.php
@@ -1,19 +1,22 @@
-<div class='directorBar'>
-    &nbsp;{{$partner->name}}
+<div class='directorWrapper'>
+    <div class='directorBar'>
+        &nbsp;{{$partner->name}}
+    </div>
+    <div class='directorInd'>
+        <br>
+        {{$partner->description}}<br><br>
+        <a class='partnerLink' href='{{$partner->website_url}}' target='_blank'>Visit partner website</a>
+        <br><br>
+
+        <form method='get' class='adminForm' action='/admin/partner/{{$partner->id}}/edit'>
+            <input type='submit' class='adminButton editButton' value='Edit'>
+        </form>
+
+        <form method='get' class='adminForm' action='/admin/partner/{{$partner->id}}/delconfirm'>
+            <input type='submit' class='adminButton deleteButton' value='Delete'>
+        </form>
+
+        <br><br>
+    </div>
 </div>
-<div class='directorInd'>
-    <br>
-    {{$partner->description}}<br><br>
-    <a class='partnerLink' href='{{$partner->website_url}}' target='_blank'>Visit partner website</a>
-    <br><br>
-
-    <form method='get' class='adminForm' action='/admin/partner/{{$partner->id}}/edit'>
-        <input type='submit' class='adminButton editButton' value='Edit'>
-    </form>
-
-    <form method='get' class='adminForm' action='/admin/partner/{{$partner->id}}/delconfirm'>
-        <input type='submit' class='adminButton deleteButton' value='Delete'>
-    </form>
-
-    <br><br>
-</div>
+<br>
